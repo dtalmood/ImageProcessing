83dbd3b3e7a5fbba62256c5515915d55df815372
dtalmood <dtalmood1@gmail.com>
2023-06-10 20:17:55 -0700

    Update Kernel

--- kernel.cu
+++ kernel.cu
@@ -1,9 +1,26 @@
-#include <stdio.h>
+#include <studio.h>
 
-__global__ void imageprocessing_kernel() {
-    
+#define IMAGE_WIDTH 800
+#define IMAGE_HEIGHT 800
+
+// Define the block size
+#define BLOCK_SIZE 16
+
+
+// Define the grid size
+dim3 gridDim( (IMAGE_WIDTH-1) / BLOCK_SIZE+1, (IMAGE_WIDTH-1) / BLOCK_SIZE+1);
+dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE);
+
+__global__ void imageprocessing_kernel() 
+{
+    // Your image processing code here
 }
 
-void imageprocessing() {
-    
+void imageprocessing() 
+{
+    // Call the kernel function with the defined grid and block dimensions
+    imageprocessing_kernel<<<gridDim, blockDim>>>();
+
+    // Ensure all the CUDA threads have finished
+    cudaDeviceSynchronize();
 }
