6265b86e1397e360d8b8d49cd73915d95278ae33
dtalmood <dtalmood1@gmail.com>
2023-06-13 14:49:12 -0700

    Update
    Importing Updated Code by Jose

--- kernel.cu
+++ kernel.cu
@@ -1,29 +1,39 @@
-#include <stdio.h>
+#include "kernel.h"
 
-struct ComparePixel {
-    __host__ __device__ bool operator()(const uchar3& pixel1, const uchar3& pixel2) const {
-        if (pixel1.z != pixel2.z) {
-            return pixel1.z < pixel2.z; // Sort by red channel
-        }
-        if (pixel1.y != pixel2.y) {
-            return pixel1.y < pixel2.y; // Sort by green channel
-        }
-        return pixel1.x < pixel2.x; // Sort by blue channel
+__device__ bool sorter(const uchar3& pixel1, const uchar3& pixel2) {
+    
+    //red channel
+    if (pixel1.z != pixel2.z) {
+        return pixel1.z < pixel2.z; 
     }
-};
+    // green channel
+    if (pixel1.y != pixel2.y) {
+        return pixel1.y < pixel2.y; 
+    }
+    // Sort by blue channel
+    return pixel1.x < pixel2.x; 
+}
 
-extern "C" __global__ void sortPixels(uchar3* pixels, int size) {
+__global__ void sortPixels(uchar3* pixels, int size) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < size) {
         uchar3 currentPixel = pixels[idx];
         for (int i = idx + 1; i < size; i++) {
             uchar3 otherPixel = pixels[i];
-            if (ComparePixel()(otherPixel, currentPixel)) {
+
+            if (sorter(otherPixel, currentPixel)) {
                 pixels[idx] = otherPixel;
                 pixels[i] = currentPixel;
                 currentPixel = pixels[idx];
             }
         }
     }
+}
 
+void pixelSort(uchar3* pixels, int size) {
+    const unsigned int BLOCK_SIZE = 256;
+    const unsigned int NUM_BLOCKS = (size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    sortPixels<<<NUM_BLOCKS, BLOCK_SIZE>>>(pixels, size);
+    cudaDeviceSynchronize();
 }
--- main.cu
+++ main.cu
@@ -1,57 +1,75 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+
+//incompatibility issues with open cv, header was needed for compilation
+#include "kernel.h"
+
 using namespace cv;
 using namespace std;
 
-// Custom comparator function for sorting pixels based on RGB values
-bool comparePixels(const Vec3b& pixel1, const Vec3b& pixel2);
-
-// Declaration of sortPixels function
-extern "C" void sortPixels(uchar3* pixels, int numPixels);
-
 int main() {
-    // Load JPEG image using OpenCV
-    // Read the image file
+    //use opencv to read image
     Mat image = imread("source.jpeg");
 
     // Check if the image was successfully loaded
     if (image.empty()) {
-        cout << "Could not open or find the image" << endl;
+        cout << "cannot read image" << endl;
         return -1;
     }
 
-    // Extract pixel values into a vector
-    vector<Vec3b> pixels;
+    //issues with opencv 2D matrix and cuda
+    //vector is used instead
+
+    vector<uchar3> pixels;
+    //traverse 2D matrix to extract pixel
     for (int y = 0; y < image.rows; y++) {
         for (int x = 0; x < image.cols; x++) {
-            pixels.push_back(image.at<Vec3b>(y, x));
+            Vec3b bgrPixel = image.at<Vec3b>(y, x);
+            uchar3 rgbPixel = make_uchar3(bgrPixel[2], bgrPixel[1], bgrPixel[0]);
+            pixels.push_back(rgbPixel);
         }
     }
 
-    // Sort the pixels based on RGB values
-    sortPixels(reinterpret_cast<uchar3*>(pixels.data()), pixels.size());
+    // Allocate host variables ----------------------------------------------
 
-    // Create a new image with sorted pixels
+    // HOST VARIABLES ARE DYNAMIC HERE
+
+    //==========================================
+
+
+    // Allocate device variables ---------------------------------------------
+
+    //incompatibility issues with opencv and cuda uchar3 is the solution
+    uchar3* d_pixels;
+    cudaMalloc((void**)&d_pixels, pixels.size() * sizeof(uchar3));
+
+
+    // Copy host variables to device ------------------------------------------
+    cudaMemcpy(d_pixels, pixels.data(), pixels.size() * sizeof(uchar3), cudaMemcpyHostToDevice);
+
+
+    // Launch kernel ---------------------------
+    pixelSort(d_pixels, pixels.size());
+
+    // Copy device variables from host ----------------------------------------
+    cudaMemcpy(pixels.data(), d_pixels, pixels.size() * sizeof(uchar3), cudaMemcpyDeviceToHost);
+
+    //building 2D opencv image
     Mat sortedImage(image.size(), image.type());
-    int index = 0;
     for (int y = 0; y < image.rows; y++) {
         for (int x = 0; x < image.cols; x++) {
-            sortedImage.at<Vec3b>(y, x) = pixels[index++];
+
+            uchar3 rgbPixel = pixels[y * image.cols + x];
+            Vec3b bgrPixel(rgbPixel.z, rgbPixel.y, rgbPixel.x);
+            sortedImage.at<Vec3b>(y, x) = bgrPixel;
         }
     }
 
+    // Save the sorted image
     imwrite("sortedPixels.jpeg", sortedImage);
-    waitKey(0);
+
+  
+    cudaFree(d_pixels);
 
     return 0;
-}
-
-bool comparePixels(const Vec3b& pixel1, const Vec3b& pixel2) {
-    if (pixel1[2] != pixel2[2]) {
-        return pixel1[2] < pixel2[2]; // Sort by red channel
-    }
-    if (pixel1[1] != pixel2[1]) {
-        return pixel1[1] < pixel2[1]; // Sort by green channel
-    }
-    return pixel1[0] < pixel2[0]; // Sort by blue channel
 }
